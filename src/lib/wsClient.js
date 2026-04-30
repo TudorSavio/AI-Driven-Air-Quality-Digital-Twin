7518b765cc0febacddefd656d5255ef3e6eb7714
TudorSavio <tudorsavio12347@gmail.com>
2026-04-30 18:30:48 +0530

    Delete src directory

--- src/lib/wsClient.js
+++
@@ -1,6 +0,0 @@
-export const testWS = () => {
-  const ws = new WebSocket("ws://localhost:4001");
-  ws.onopen = () => console.log("WS TEST: CONNECTED");
-  ws.onerror = (e) => console.log("WS TEST: ERROR", e);
-  ws.onclose = () => console.log("WS TEST: CLOSED");
-};
